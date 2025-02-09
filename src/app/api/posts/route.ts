ce242ef4724bae0aaaca568461a3eb58dfb8ac96
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-02-09 18:58:22 +0900

    fix: remove comment

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,31 +1,31 @@
-// import * as contentful from "contentful";
-// import { NextResponse } from "next/server";
+import type * as contentful from "contentful";
+import { NextResponse } from "next/server";
 
-// import client from "@/lib/client";
+import client from "@/lib/client";
 
-// export type GalleryPostSkeleton = {
-//   contentTypeId: "ayaGallery";
-//   fields: {
-//     photo: contentful.EntryFieldTypes.AssetLink;
-//     tags: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
-//     category: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
-//     uploadAt: contentful.EntryFieldTypes.Date;
-//   };
-// };
+export type GalleryPostSkeleton = {
+  contentTypeId: "ayaGallery";
+  fields: {
+    photo: contentful.EntryFieldTypes.AssetLink;
+    tags: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
+    category: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
+    uploadAt: contentful.EntryFieldTypes.Date;
+  };
+};
 
-// export type BlogPostsData = contentful.EntryCollection<GalleryPostSkeleton>;
+export type BlogPostsData = contentful.EntryCollection<GalleryPostSkeleton>;
 
-// export async function GET(req: Request): Promise<NextResponse<BlogPostsData>> {
-//   const { searchParams } = await new URL(req.url);
-//   const limit = Number(searchParams.get("limit") || 0);
-//   const skip = Number(searchParams.get("skip") || 0);
+export async function GET(req: Request): Promise<NextResponse<BlogPostsData>> {
+  const { searchParams } = await new URL(req.url);
+  const limit = Number(searchParams.get("limit") || 0);
+  const skip = Number(searchParams.get("skip") || 0);
 
-//   const entries = await client.getEntries<GalleryPostSkeleton>({
-//     content_type: "ayaGallery",
-//     order: ["-fields.uploadAt"],
-//     limit,
-//     skip,
-//   });
+  const entries = await client.getEntries<GalleryPostSkeleton>({
+    content_type: "ayaGallery",
+    order: ["-fields.uploadAt"],
+    limit,
+    skip,
+  });
 
-//   return NextResponse.json(entries);
-// }
+  return NextResponse.json(entries);
+}
