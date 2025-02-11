c4c757d8ff0e297df637e47ff7cc9c1e84c44a30
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-02-11 23:49:47 +0900

    fix: routing process and add tags route

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,34 +1,19 @@
+import client from "@/lib/client";
+import type { GalleryPostSkeleton } from "@/types/contentful";
 import type * as contentful from "contentful";
 import { NextResponse } from "next/server";
 
-import client from "@/lib/client";
-
-export type GalleryPostSkeleton = {
-  contentTypeId: "ayaGallery";
-  fields: {
-    title: contentful.EntryFieldTypes.Text;
-    photo: contentful.EntryFieldTypes.AssetLink;
-    tags: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
-    category: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.Symbol>;
-    uploadAt: contentful.EntryFieldTypes.Date;
-    json: contentful.EntryFieldTypes.Object;
-  };
-};
-
-//export type BlogPostData = contentful.Entry<GalleryPostSkeleton>;
-export type BlogPostsData = contentful.EntryCollection<GalleryPostSkeleton>;
+export type BlogPostData = contentful.Entry<GalleryPostSkeleton>;
 
 export async function GET(
   _: Request,
   props: { params: Promise<{ id: string }> },
-): Promise<NextResponse<BlogPostsData>> {
+): Promise<NextResponse<BlogPostData>> {
   const params = await props.params;
 
   const { id } = params;
 
-  const entry = await client.getEntries<GalleryPostSkeleton>({
-    "sys.id[in]": id.split(","),
-  });
+  const entry = await client.getEntry<GalleryPostSkeleton>(id);
 
   return NextResponse.json(entry);
 }
--- src/app/api/tags/[tag]/route.ts
+++ src/app/api/tags/[tag]/route.ts
@@ -3,16 +3,22 @@ import type { GalleryPostSkeleton } from "@/types/contentful";
 import type * as contentful from "contentful";
 import { NextResponse } from "next/server";
 
-export type BlogPostsData = contentful.EntryCollection<GalleryPostSkeleton>;
+export type PostsDataContainTag =
+  contentful.EntryCollection<GalleryPostSkeleton>;
 
-export async function GET(req: Request): Promise<NextResponse<BlogPostsData>> {
+export async function GET(
+  req: Request,
+  props: { params: Promise<{ tag: string }> },
+) {
   const { searchParams } = await new URL(req.url);
+  const tag = (await props.params).tag;
+
   const limit = Number(searchParams.get("limit") || 15);
   const skip = Number(searchParams.get("skip") || 0);
 
-  const entries = await client.getEntries<GalleryPostSkeleton>({
+  const entries = await client.getEntries({
     content_type: "ayaGallery",
-    order: ["-fields.uploadAt"],
+    "metadata.tags.sys.id[in]": [tag],
     limit,
     skip,
   });
