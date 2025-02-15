1928c0e6bd25829b195198555e1986d0f76d839a
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-02-15 17:08:25 +0900

    fix: contentful initialize

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 const client = createClient({
   accessToken: process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN || "",
-  environment: process.env.CONTENTFUL_ENVIRONMENT,
+  environment: process.env.CONTENTFUL_ENVIRONMENT || "",
   space: process.env.CONTENTFUL_SPACE_ID || "",
 });
 
