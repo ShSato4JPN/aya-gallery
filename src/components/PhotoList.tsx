875081ff0a40fc1e79da06b367d9b55f8aa4fe41
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-03-04 23:50:00 +0900

    fix: layout of tags gallery

--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -33,8 +33,8 @@ export default function PhotoList({ photos }: PhotoListProps) {
     const styles = [
       [
         "relative w-full h-80 col-start-1 col-end-3 md:h-full md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-3",
-        "relative w-full h-52 col-start-1 col-end-3 md:h-64 md:h-64 md:col-start-3 md:col-end-5",
-        "relative w-full h-52 col-start-1 col-end-2 md:h-64 md:h-64 md:col-start-5 md:col-end-6",
+        "relative w-full h-52 col-start-1 col-end-3 md:h-64 md:col-start-3 md:col-end-5",
+        "relative w-full h-52 col-start-1 col-end-2 md:h-64 md:col-start-5 md:col-end-6",
         "relative w-full h-52 col-start-2 col-end-3 md:h-64 md:col-start-3 md:col-end-4",
         "relative w-full h-52 col-start-1 col-end-3 md:h-64 md:col-start-4 md:col-end-6",
       ],
@@ -61,6 +61,59 @@ export default function PhotoList({ photos }: PhotoListProps) {
       ],
     ][countStart % 4];
 
+    // 急遽レイアウト崩れ対応策に用意したスタイル
+    const subStyles = [
+      [
+        "relative w-full h-80 col-start-1 col-end-3 md:h-92 md:col-start-2 md:col-end-5",
+      ],
+      [
+        "relative w-full h-80 col-start-1 col-end-3 md:h-64 md:col-start-1 md:col-end-3",
+        "relative w-full h-80 col-start-1 col-end-3 md:h-64 md:col-start-3 md:col-end-4",
+      ],
+      [
+        "relative w-full h-80 col-start-1 col-end-3 md:h-full md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-3",
+        "relative w-full h-80 col-start-1 col-end-2 md:h-64 md:col-start-3 md:col-end-5",
+        "relative w-full h-80 col-start-2 col-end-3 md:h-64 md:col-start-3 md:col-end-4",
+      ],
+      [
+        "relative w-full h-80 col-start-1 col-end-3 md:h-64 md:col-start-1 md:col-end-2",
+        "relative w-full h-52 col-start-1 col-end-3 md:h-64 md:col-start-2 md:col-end-4",
+        "relative w-full h-52 col-start-1 col-end-2 md:h-64 md:col-start-4 md:col-end-5",
+        "relative w-full h-52 col-start-2 col-end-3 md:h-64 md:col-start-5 md:col-end-6",
+      ],
+    ][group.length - 1];
+
+    if (group.length < 5) {
+      return (
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-5 md:gap-3">
+          {group.map((photo, index) => (
+            <motion.div
+              key={`${photo.url}`}
+              onClick={() => setSelectedImage(photo)}
+              className={`${subStyles.at(
+                index % itemCount,
+              )} overflow-hidden cursor-pointer rounded-md`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                delay: (countStart + index) * 0.2,
+                duration: 0.5,
+                ease: "easeOut",
+              }}
+            >
+              <Image
+                className="object-cover hover:scale-110 hover:brightness-125 transition duration-700"
+                src={photo.url}
+                alt={photo.title}
+                fill={true}
+                quality={100}
+              />
+            </motion.div>
+          ))}
+        </div>
+      );
+    }
+
     return (
       <div className="grid grid-cols-2 gap-2 md:grid-cols-5 md:gap-3">
         {group.map((photo, index) => (
@@ -68,7 +121,7 @@ export default function PhotoList({ photos }: PhotoListProps) {
             key={`${photo.url}`}
             onClick={() => setSelectedImage(photo)}
             className={`${styles.at(
-              index % itemCount
+              index % itemCount,
             )} overflow-hidden cursor-pointer rounded-md`}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
